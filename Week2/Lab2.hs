0243cd9cd6c5e0f78b827f603bb8a9d86afd8686
Sinan Ceylan <Sinan@048.068.092.145.hva.nl>
2014-09-11 11:46:20 +0200

    Fixes to the Triangle script.

--- Week2/Lab2.hs
+++ Week2/Lab2.hs
@@ -8,27 +8,15 @@ data Shape = NoTriangle | Equilateral
 
 
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle a b c  | a <= 0 || b<= 0 || c<=0 = NoTriangle
+triangle a b c  | a <= 0 = NoTriangle
                 | a + b < c = NoTriangle
                 | a + b == c = Scalene
-                | a==b&&b==c = Equilateral
+                | a==c = Equilateral
                 | (a^2) + (b^2) == c^2 = Rectangular
-                | a == b || b == c || c == a = Isosceles
+                | a == b || b == c = Isosceles
                 | otherwise = Scalene
 
 
-trianglep :: Integer -> Integer -> Integer -> Shape
---sortlegs x y z  | sort [x,y,z] = (x,y,z)
-trianglep x y z = triangle (s!!0) (s!!1) (s!!2) where s = sort [x,y,z]
+sortTriangle :: Integer -> Integer -> Integer -> Shape
 
-
-
---sortlegs1 :: Integer -> Integer -> Integer ->[Integer]
---sortlegs x y z  | sort [x,y,z] = (x,y,z)
---sortlegs1 x y z = [(s!!0), (s!!1), (s!!2)] where s = sort [x,y,z]
-
-
---triangle sort[x,y,z]
---s!!0 s!!1 s!!2
---trianglep :: Integer -> Integer -> Integer -> Shape
---trianglep a b c  | s!!0 + s!!1 <= s!!2 where s = sort[a,b,c] =
+sortTriangle x y z = triangle (s!!0) (s!!1) (s!!2) where s = sort [x,y,z]
