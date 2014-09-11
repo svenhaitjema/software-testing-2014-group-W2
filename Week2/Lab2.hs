bcb943588a3766f56db01ae8208094b2c442a988
Sinan Ceylan <Sinan@048.068.092.145.hva.nl>
2014-09-11 16:04:26 +0200

    Added assignment two. Works and has been tested. Call isPermutation with two lists to check.

--- Week2/Lab2.hs
+++ Week2/Lab2.hs
@@ -1,12 +1,13 @@
-module Lab2 where
+module Week2 where
 
 import Data.List
+import Data.Eq
 import System.Random
 
 data Shape = NoTriangle | Equilateral
             | Isosceles | Rectangular | Scalene deriving (Eq,Show)
 
-
+-- 1.
 triangle :: Integer -> Integer -> Integer -> Shape
 triangle a b c  | a <= 0 = NoTriangle
                 | a + b < c = NoTriangle
@@ -20,3 +21,19 @@ triangle a b c  | a <= 0 = NoTriangle
 sortTriangle :: Integer -> Integer -> Integer -> Shape
 
 sortTriangle x y z = triangle (s!!0) (s!!1) (s!!2) where s = sort [x,y,z]
+
+isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation x y | length(x) /= length(y) = False
+                  | otherwise = permuCheck x y
+
+permuCheck :: Eq a => [a] -> [a] -> Bool
+permuCheck [] [] = True
+permuCheck (x:xs) (y:ys) | (x:xs) == (y:ys) = True
+                         | elem x (y:ys) == False = False
+                         | elem x (y:ys) == True = permuCheck xs (removeItem x (y:ys))
+                         | otherwise = False
+
+removeItem :: Eq a => a -> [a] -> [a]
+removeItem _ [] = []
+removeItem x (y:ys) | x == y = removeItem x ys
+                    | otherwise = y : removeItem x ys
