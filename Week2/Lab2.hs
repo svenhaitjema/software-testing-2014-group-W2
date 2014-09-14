32590645fbce17a47902b2e08ffdae26a236ef52
amirico <thatsmyemail.com@gmail.com>
2014-09-14 13:35:02 +0300

    Update Lab2.hs

--- Week2/Lab2.hs
+++ Week2/Lab2.hs
@@ -47,4 +47,6 @@ isDerangement [] [] = True
 isDerangement (x:xs) (y:ys) | x == y = False
 	| otherwise = isDerangement xs ys
 
+deran :: (Enum a, Eq a, Num a) => a -> [[a]]
+deran n = [i|i<-p,j<-p,and(zipWith(/=) i j)] where p = perms[0..(n-1)]
                    
