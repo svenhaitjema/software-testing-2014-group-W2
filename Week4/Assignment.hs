952a65c307d016de983ed73445ee201e6711d770
Sinan Ceylan <Sinan@106.068.092.145.hva.nl>
2014-09-24 16:49:52 +0200

    Changes to the random generator.

--- Week4/Assignment.hs
+++ Week4/Assignment.hs
@@ -11,7 +11,7 @@
 --    -
 --    -
 --    -
---Wai  Yi:
+--Wai Yi:
 --    -
 --    -
 --    -
@@ -28,5 +28,22 @@ module Assignment
 
 where
 
+import Test.QuickCheck
 import System.Random
 import Data.List
+import SetOrd
+
+getRandomInt :: Int -> IO Int
+getRandomInt n = getStdRandom (randomR (0,n))
+
+getRandomSet :: Int -> Set -> IO(Set Int)
+getRandomSet i s | isEmpty(s) = do
+                                    p <- getRandomInt(i)
+                                    return getRandomSet(p (insertSet p emptySet))
+                 | length(s) < 10 =  do
+                                        h <- getRandomInt(i)
+                                        return getRandomSet(h (insertSet h s))
+                 | otherwise = s
+
+
+                 Jongens, bekijk even de code wat ik hierhoven heb. Volgens mij zit ik er heel dichtbij, maar is dit nog niet helemaal goed.
