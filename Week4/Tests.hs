dcdb5d19997908915ac868346bd4ee56da3518b1
Wai Yi Leung <w.y.leung@e-sensei.nl>
2014-09-30 15:47:10 +0200

    more test, check wheter output elements where also in the input, (written by Sinan)

--- Week4/Tests.hs
+++ Week4/Tests.hs
@@ -1,14 +1,31 @@
 module Tests where
 
+import Data.List
+import SetOrd
+import System.Random
 import Test.Hspec
+import Test.QuickCheck
 import Assignment
 
 main = hspec testtrClos
 
+verif :: Rel Int -> [Bool] -- Rel Int
+verif [] = []
+verif (x:xs) = [elem x (trClos(x:xs))] ++ (verif xs)
+
+
+
 testtrClos = describe "trClos" $ do
     it "Following assignment" $ do
         (trClos [(1,2),(2,3),(3,4)] == [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)])
+--    it "Empty set" $ do
+--        (trClos [] == [])
     it "Single item in set, should not give transitive set" $ do
         (trClos [(0,1)] == [(0,1)])
     it "Simple" $ do
         (trClos [(0,1),(1,0)] == [(0,0),(0,1),(1,0),(1,1)])
+    it "Indirect" $ do
+        (trClos [(0,1),(1,2),(2,1)] == [(0,1),(0,2),(1,1),(1,2),(2,1),(2,2)])
+    it "includes all elements from input in the output" $ do
+        property $ \x -> 
+            all (==True) (verif (nub(x)))
