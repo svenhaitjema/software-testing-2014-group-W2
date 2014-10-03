eb521c20ed943475b45fa2fafbb1d784e811bd3f
amirico <thatsmyemail.com@gmail.com>
2014-10-03 11:50:46 +0300

    Update Tests.hs

--- Week4/Tests.hs
+++ Week4/Tests.hs
@@ -19,18 +19,18 @@ main = hspec testtrClos
 testtrClos = describe "trClos" $ do
     it "Following assignment" $ do
         (trClos [(1,2),(2,3),(3,4)] == [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)])
-    -- it "gives the transitive closure of binary relations" $ do 
-    --     property $ \x -> 
-    --         trClos x == bcd (x)
-    -- it "includes all elements from input in the output" $ do
-    --     property $ \x -> 
-    --         all (==True) (verif (nub(x)))    
+     it "gives the transitive closure of binary relations" $ do 
+         property $ \x -> 
+             trClos x == bcd (x)
+     it "includes all elements from input in the output" $ do
+         property $ \x -> 
+             all (==True) (verif (nub(x)))    
     it "Single item in set, should not give transitive set" $ do
         (trClos [(0,1)] == [(0,1)])
     it "Simple" $ do
         (trClos [(0,1),(1,0)] == [(0,0),(0,1),(1,0),(1,1)])
     it "Indirect" $ do
         (trClos [(0,1),(1,2),(2,1)] == [(0,1),(0,2),(1,1),(1,2),(2,1),(2,2)])
---    it "includes all elements from input in the output" $ do
---        property $ \x -> 
---            all (==True) (verif (nub(x)))
+    it "includes all elements from input in the output" $ do
+        property $ \x -> 
+            all (==True) (verif (nub(x)))
