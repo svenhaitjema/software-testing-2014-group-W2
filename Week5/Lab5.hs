ed7ebb14e5f569ae0a817b54b0939d843ec22810
amirico <thatsmyemail.com@gmail.com>
2014-10-17 00:00:49 +0200

    Update Lab5.hs

--- Week5/Lab5.hs
+++ Week5/Lab5.hs
@@ -213,6 +213,40 @@ prune (r,c,v) ((x,y,zs):rest)
 -}
 
 -- Question 5
+
+A complete NRCSudoku problem & solution in the file NRCSudoku.hs, (at the very bottom of the file, namely the following:)
+
+
+
+example6 :: Grid		  
+example6 = [[0,0,0,3,0,0,0,0,0],
+            [0,0,0,7,0,0,3,0,0],
+            [2,0,0,0,0,0,0,0,8],
+            [0,0,6,0,0,5,0,0,0],
+            [0,9,1,6,0,0,0,0,0],
+            [3,0,0,0,7,1,2,0,0],
+            [0,0,0,0,0,0,0,3,1],
+            [0,8,0,0,4,0,0,0,0],
+            [0,0,2,0,0,0,0,0,0]]
+		  
+		  
+solution6 :: Grid
+solution6 = [[4,7,8,3,9,2,6,1,5],
+             [6,1,9,7,5,8,3,2,4],
+             [2,3,5,4,1,6,9,7,8],
+             [7,2,6,8,3,5,1,4,9],
+             [8,9,1,6,2,4,7,5,3],
+             [3,5,4,9,7,1,2,8,6],
+             [5,6,7,2,8,9,4,3,1],
+             [9,8,3,1,4,7,5,6,2],
+             [1,4,2,5,6,3,8,9,7]]
+
+
+
+
+
+
+
 genPrSudoku ::  IO()
 genPrSudoku = do
 	[sudoku] <- rsolveNs [emptyN]
